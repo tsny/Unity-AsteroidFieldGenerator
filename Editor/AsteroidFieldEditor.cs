ec6fec315990780df331f670476f125bd8abedf7
tsny <tsny700@gmail.com>
2018-09-04 22:18:25 -0500

    Init Commit My Itty Bitty Scripts

---
+++ Editor/AsteroidFieldEditor.cs
@@ -0,0 +1,50 @@
+﻿using UnityEditor;
+using UnityEngine;
+using EGL = UnityEditor.EditorGUILayout;
+
+[CustomEditor(typeof(AsteroidField))]
+public class AsteroidFieldEditor : Editor 
+{
+    public bool scaleUsesRange;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        EGL.Space();
+
+        AsteroidField field = (AsteroidField)target;
+
+        field.AsteroidGameObject = (GameObject) EGL.ObjectField(new GUIContent("Asteroid Game Object", "The game object that represents the spawned asteroids"), field.AsteroidGameObject, typeof(GameObject), false);
+
+        field.InnerRadius = EGL.FloatField(new GUIContent("Inner Radius", "The inner radius of the field"), field.InnerRadius);
+        field.OuterRadius = EGL.Slider("Outer Radius", field.OuterRadius, field.InnerRadius, 10000 + field.InnerRadius);
+
+        EGL.Space();
+
+        scaleUsesRange = EGL.Toggle(new GUIContent("Scale uses range?", "Whether the asteroids spawned should pick a scale from a specified range."), scaleUsesRange);
+
+        if (scaleUsesRange)
+        {
+            field.AsteroidLowerScale = EGL.Slider("Lower range", field.AsteroidLowerScale, 0, 20);
+            field.AsteroidUpperScale = EGL.Slider("Upper range", field.AsteroidUpperScale, field.AsteroidLowerScale, field.AsteroidLowerScale + 20);
+        }
+
+        else
+        {
+            field.AsteroidScale = EGL.Slider("Scale", field.AsteroidScale, 0, 20);
+        }
+
+        using (new EditorGUI.DisabledScope(field.AsteroidGameObject == null))
+        {
+            if(GUILayout.Button("Generate Field"))
+            {
+                field.GenerateField(scaleUsesRange);
+            }
+        }
+
+        if(GUILayout.Button("Clear Field"))
+        {
+            field.ClearField();
+        }
+    }
+}
---
+++ Scripts/AsteroidField.cs
@@ -0,0 +1,79 @@
+﻿using System.Linq;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class AsteroidField : MonoBehaviour
+{
+    public GameObject AsteroidGameObject { get; set; }
+
+    [Space(10)]
+    [Tooltip("Select 'Hide In Hierarchy' to ensure a cleaner hierarchy")]
+    public new HideFlags hideFlags;
+
+    public int desiredAsteroids = 1000;
+
+    public float InnerRadius { get; set; }
+    public float OuterRadius { get; set; }
+    public float AsteroidScale { get; set; }
+    public float AsteroidLowerScale { get; set; }
+    public float AsteroidUpperScale { get; set; }
+
+    public void GenerateField(bool scaleUsesRange)
+    {
+        ClearField();
+        CreateAsteroids(scaleUsesRange);
+    }
+
+    private void CreateAsteroids(bool scaleUsesRange)
+    {
+        if (scaleUsesRange)
+        {
+            for (int i = 0; i < desiredAsteroids; i++)
+            {
+                CreateAsteroid(AsteroidLowerScale, AsteroidUpperScale);
+            }
+        }
+
+        else
+        {
+            for (int i = 0; i < desiredAsteroids; i++)
+            {
+                CreateAsteroid(AsteroidScale);
+            }
+        }
+    }
+
+    private GameObject CreateAsteroid(float scale = 1)
+    {
+        Vector3 newPos;
+        Quaternion newRot;
+        Vector3 newScale = Vector3.one * scale;
+
+        GameObject asteroid = Instantiate(AsteroidGameObject);
+
+        asteroid.hideFlags = hideFlags;
+
+        asteroid.transform.parent = gameObject.transform;
+        asteroid.transform.localScale = newScale;
+
+        newPos = transform.position + Random.onUnitSphere * Random.Range(InnerRadius, OuterRadius);
+        newRot = Random.rotation;
+
+        asteroid.transform.SetPositionAndRotation(newPos, newRot);
+
+        return asteroid;
+    }
+
+    private GameObject CreateAsteroid(float lowerScale = 1, float upperScale = 2)
+    {
+        float randomScale = Random.Range(lowerScale, upperScale);
+
+        return CreateAsteroid(randomScale);
+    }
+
+    public void ClearField()
+    {
+        var tempList = transform.Cast<Transform>().ToList();
+        foreach (Transform transform in tempList) DestroyImmediate(transform.gameObject);
+    }
+}
